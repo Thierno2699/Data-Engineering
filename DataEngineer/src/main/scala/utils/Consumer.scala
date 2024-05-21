b556ae5272ed8acc781c6d7512c6ee60ac2a7ef8
Thierno269 <thierno09.diallo@gmail.com>
2024-05-21 20:49:51 +0200

    correctif dans le consumer

--- DataEngineer/src/main/scala/utils/Consumer.scala
+++ DataEngineer/src/main/scala/utils/Consumer.scala
@@ -3,7 +3,7 @@ package utils
 import java.util.Properties
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 import scala.collection.JavaConverters._
-
+import scala.util.{Try, Success, Failure}
 object Consumer {
   def main(args: Array[String]): Unit = {
     val props = new Properties()
@@ -13,18 +13,24 @@ object Consumer {
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest") // Définir le décalage sur le début du topic
 
-    val consumer = new KafkaConsumer[String, String](props)
-    consumer.subscribe(List("drone_data").asJava) // Souscrire au topic "DroneData"
 
-    try {
-      while (true) {
-        val records = consumer.poll(java.time.Duration.ofMillis(100))
-        for (record <- records.asScala) {
+    val consumer = new KafkaConsumer[String, String](props)
+    consumer.subscribe(List("drone_data").asJava)
+
+    processRecords(consumer)
+  }
+
+  def processRecords(consumer: KafkaConsumer[String, String]): Unit = {
+    Try(consumer.poll(java.time.Duration.ofMillis(100))) match {
+      case Success(records) =>
+        records.asScala.foreach { record =>
           println(s"Clé: ${record.key()}, Valeur: ${record.value()}, Offset: ${record.offset()}, Partition: ${record.partition()}")
         }
-      }
-    } finally {
-      consumer.close()
+        processRecords(consumer) // Récursivité pour continuer le traitement
+
+      case Failure(exception) =>
+        println(s"Erreur lors de la réception des données: ${exception.getMessage}")
+        consumer.close() // Fermer le consommateur proprement en cas d'échec
     }
   }
 }
